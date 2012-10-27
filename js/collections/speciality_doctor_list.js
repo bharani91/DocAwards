d0deda65c8cfd330e0ed0aa1af814eddde7a763a
Nikhil Sama <nikhilsama@gmail.com>
2012-10-27 19:34:48 +0530

    fix specialtis filter

--- js/collections/speciality_doctor_list.js
+++ js/collections/speciality_doctor_list.js
@@ -5,12 +5,12 @@ define([
   'collections/doctor_list'
 ], function($, _, Backbone, DoctorList){
   var SpecialityDoctorList = DoctorList.extend({
-    // initialize: function(options) {
-    //   this.id = options.id;
-    // },
+    initialize: function(options) {
+       this.id = options.id;
+     },
 
     url: function() {
-      return "http://docawards.com/api/doctors/get_doctors.json?brief=1&speciality=" + this.id
+      return "http://docawards.com/api/doctors/get_doctors.json?brief=1&specialty_id=" + this.id
     },
   });
 
