a1df830bf61e53c36f675d6ba9a6b0396d8f5892
Bharani M <bharani91@gmail.com>
2012-11-02 23:10:11 +0530

    Minor changes

--- js/views/doctor/profile_form.js
+++ js/views/doctor/profile_form.js
@@ -2,37 +2,130 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'views/shared/header',
-  'views/shared/footer',
-  'views/home/main',
-  'models/doctor',
-  'views/doctor/main',
-  'collections/disease_doctor_list',
-  'collections/speciality_doctor_list',
-  'views/doctor/list',
-  'views/doctor/create_profile',
-  'views/doctor/profile_form',
   'models/form_data',
-  'models/user',
-  'views/home/faq',
-  'collections/datas'
+  'models/doctor',
+  'text!templates/doctor/personal_details.html',
+  'text!templates/doctor/specializations.html',
+  'text!templates/doctor/qualifications.html',
+  'text!templates/doctor/experiences.html',
+  'text!templates/doctor/consultation.html',
+  'text!templates/doctor/contact_details.html',
+  'text!templates/partials/qualification_field.html',
+  'text!templates/partials/experience_field.html',
+  'text!templates/partials/consultation_field.html',
+  'text!templates/partials/contact_field.html',
+  'text!templates/partials/add_location_modal.html',
+  'text!templates/partials/add_specializations_modal.html',
+  'text!templates/partials/add_degree_modal.html',
+  'text!templates/partials/add_city_modal.html',
+  'text!templates/partials/add_country_modal.html',
+  'text!templates/partials/add_pin_code_modal.html'
 
-], function($, _, Backbone, HeaderView, FooterView, mainHomeView, Doctor, DoctorView, DiseaseDoctorList, SpecialityDoctorList, DoctorListView, CreateProfileView, ProfileFormView, FormData, User, FaqView, DataCollection){
-  var AppRouter = Backbone.Router.extend({
-    routes: {
-      ""                    :       "home",
-      "doctor/:id"          :       "doctorProfile",
-      "disease/:id"         :       "diseaseListing",
-      "speciality/:id"      :       "specialityListing",
-      "create_profile"      :       "createProfile",
-      "create_profile/:id"  :       "createProfileTab",
-      "faq"                 :       "faq"
-    },
+], function($, _, Backbone, FormData, Doctor, personal_details_template, specializations_template, qualifications_template, 
+            experiences_template, consultation_template, contact_details_template, qualification_field, experience_field, 
+            consultation_field, contact_field, add_location_modal, add_specializations_modal, add_degrees_modal, 
+            add_cities_modal, add_countries_modal, add_pin_code_modal){
+    var ProfileFormView = Backbone.View.extend({
+      initialize: function(options) {
+        this.el = options.el;
+        this.form_type = options.template.split("_template")[0];
+        this.template = _.template(eval(options.template));
 
-    initialize: function() {
+        // Initialize all field counts to zero
+        this.field_count = {
+          "qualification_field" : -1,
+          "experience_field" : -1,
+          "experience_field" : -1,
+          "consultation_field" : -1,
+          "contact_field" : -1,
+        }; 
 
-      // Get Current User id
-      if(!window.current_user) {
+        if(window.current_user) {
+          this.check_existing_doctor();  
+        }
+
+        this.render();
+        
+        var add_location_template = _.template(add_location_modal),
+            add_specializations_template = _.template(add_specializations_modal),
+            add_degrees_template = _.template(add_degrees_modal),
+            add_cities_template = _.template(add_cities_modal),
+            add_countries_template = _.template(add_countries_modal),
+            add_pin_codes_template = _.template(add_pin_code_modal);
+        
+        $(this.el).append(add_location_template).append(add_specializations_template).append(add_degrees_template).append(add_cities_template).append(add_countries_template).append(add_pin_codes_template);
+      },
+
+      events: {
+        "click .prev"                   :              "prev",
+        "click .next"                   :              "saveTab",
+        "click .submit"                 :              "submit",
+        "submit .add_new_entry"         :              "add_new_entry",
+        "click .add_another"            :              "add_another_field"  
+      },
+
+      render: function() {
+        
+        this.preload_tab_data(this.form_type);
+        $(this.el).append(this.template());
+        this.form = $(this.el).find(".primary");
+        
+        // Check if the string ends with s, if not just append _field
+        var form_type = this.form_type.split("_")[0],
+            field = (form_type.charAt(form_type.length - 1) == "s") ? form_type.slice(0, -1) + "_field" : form_type + "_field",
+            count = this.field_count[field];        
+        console.log(field);
+        if(count == -1) {
+          this.add_another_field(field);
+        }
+      },
+      prev: function(evt) {
+      },
+
+      saveTab: function(evt) {
+        var model = new FormData($(this.form).serializeFormJSON());
+        model.set({"form_type": this.form_type});
+        //delete preivously created model (if any)
+        var that = this;
+        old_model = this.collection.filter(function(model){ 
+          return model.attributes.form_type === that.form_type; 
+        });
+        this.collection.remove(old_model);
+
+        //save this model data        
+        this.collection.add(model);
+
+      },
+
+      submit: function() {
+        this.saveTab();
+        var data = {};
+        _.each(this.collection.models, function(model) {
+          var type = model.get("form_type"), m = model.toJSON();
+          console.log(m)
+          if(type == "personal_details") {
+            for(key in m) {
+              if (key == "form_type") return;
+              data[key] = m[key]; 
+            }
+
+          } else if(type == "specializations") {
+            _.each(m["data[Docspeclink][0][specialty_id]"], function(spec_link, index) {
+              data["data[Docspeclink][" + index +"][specialty_id]"] = spec_link;
+            });
+          } else {
+            _.extend(data, m)
+          }
+
+        });
+
+        // Replace with current user
+        data["data[Doctor][user_id]"] = window.current_user.get("id");
+        data["data[Doctor][image]"] = "temp.jpg";
+
+        console.log("DATA", data);
+        window.temp_data = data;
+
         $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
           options.xhrFields = {
             withCredentials: true
@@ -40,151 +133,193 @@ define([
         });
 
         $.ajax({
-          type: 'GET',
-          url: "http://docawards.com/api/users/get_user.json",
-          dataType: "json",
-        
+          type: 'POST',
+          url: "http://docawards.com/api/doctors/ws_add.json",
+          data: data,
           success: function(data) {
-            if (data.code == '200') {
-              if(data.data.status > 0 && data.data["Doctor"]) {
-                window.current_user = new Doctor({ id: data.data["Doctor"][0]["id"]});
+            console.log(data);
+            alert("Saved!");
+          }
+        });
+
+      },
+
+      put_into_form: function(type) {
+        var that = this;
+        
+        // Check if the string ends with s, if not just append _field
+        var form_type = this.form_type.split("_")[0],
+            field = (form_type.charAt(form_type.length - 1) == "s") ? form_type.slice(0, -1) + "_field" : form_type + "_field",
+            count = this.field_count[field];  
+
+        for(var i = 0; i <= count && count >= 0 ; i++) {  
+          this.add_another_field(field);
+        }
+
+        setTimeout(function() {
+          $this = $(that.el).parent().find("li.active form.primary");
+          if(form_type == "specializations") {
+            console.log(type['data[Docspeclink]'].split(", "))
+            $(".multiple_select").val(type['data[Docspeclink]'].split(", "));
+            $(".multiple_select").trigger('liszt:updated');
+
+          } else {
+            for(key in type) {
+              $this.find("input[name='" + key + "']").val(type[key]);
+              $this.find("select[name='"+key+"']").val(type[key]);
+              $this.find("select[name='"+key+"']").trigger('liszt:updated');
+            }
+          }  
+        }, 100)
+
+
+
+        
+      },
+
+      preload_tab_data: function(type) {
+        //Preload the form for the next tab if a model exists for it
+        //Model would exist if the user has saved something then clicked next
+        var form_type = type;
+        model = this.collection.filter(function(model){
+          return model.attributes.form_type == form_type;
+        });
+        if (model && model[0]) {
+          this.put_into_form(model[0].toJSON());
+        }
+      },
+
+      add_new_entry: function(evt) {
+        var $form = $(evt.target),
+            data = $form.serializeFormJSON();
+        $.ajaxPrefilter( function( options, originalOptions, jqXHR ) {
+          options.xhrFields = {
+            withCredentials: true
+          };
+        });
+
+        $.ajax({
+          type: 'POST',
+          url: "http://docawards.com/api/"+$form.data('servermodel')+"/ws_add.json",
+          data: data,
+          success: function(data) {
+            console.log(data);
+            if (data.code = '200') {
+              alert("Saved!");
+              select = $($form.data('targetselect'));
+              for(var key in data.data) {
+                select.append('<option selected="selected" value='+key+'>'+data.data[key]+'</option>');
               }
+              select.trigger("liszt:updated");
+              $form.parent().siblings('.close-reveal-modal').trigger('click');
             }
           }
         });
-      }
+        return false;
+      },
 
-    },
-
-    home: function(){
-      var home_view = new mainHomeView();
-      // hide regular header
-      $(".header_wrapper").hide();
-      // if(window.location.search.indexOf("?") >= 0) {
-      //   var queries = {};
-      //   $.each(window.location.search.substr(1).split('&'),function(c,q){
-      //     var i = q.split('=');
-      //     queries[i[0].toString()] = i[1].toString();
-      //   });
-
-      //   window.current_user = new User({id: queries["user_id"]});
-      //   window.location.search = "";
-      // } 
-      
-    },
-
-    doctorProfile: function(id)  {
-      var doctor = new Doctor({id: id});
-      var doctor_view = new DoctorView({model: doctor});
-    },
-
-    diseaseListing: function(id) {
-      var doctor_list_view = new DoctorListView({collection: new DiseaseDoctorList({ id: id })});
-    },
-
-    specialityListing: function(id) {
-      var doctor_list_view = new DoctorListView({collection: new SpecialityDoctorList({ id: id })});
-    },
-
-    createProfile: function() {
-      var header_view = new HeaderView();
-      $(".header_wrapper").fadeIn("slow");
-
-
-      window.form_data = [];
-      
-      var create_profile_view = new CreateProfileView();
-
-      $(".wrapper").hide().fadeIn("slow", function() {
-
-        window.datas = new DataCollection();
-        var form_view = new ProfileFormView({ collection: window.datas, url: "personal_details", el: "li#personal_detailsTab", template: "personal_details_template"});
-        //Datepicker
-        $( ".datepicker" ).datepicker({
-            changeMonth: true,
-            changeYear: true,
-            yearRange: "1910:2012",
-            defaultDate: "-10y",
-            dateFormat: 'dd-mm-yy'
-        });
-        $( ".timepicker" ).timepicker({
-          showPeriod: true,
-          showLeadingZero: true
-        });
-
-        $(".chosen_simple").chosen();
-      });
-      
-
-    },
-
-    createProfileTab: function(id) {
-      var header_view = new HeaderView(),
-          create_profile_view = new CreateProfileView();
-      var el = "li#" + id + "Tab",
-          template = id + "_template";
+      add_another_field: function(evt) {
+        // Check Type to see if the function is called on 'click' or on load
+        if(typeof evt == "string") {
+          var elem = evt,
+          target = $("a[data-elem='" + elem + "']");
+        } else {
+          var elem = $(evt.target).data("elem"),
+              target = $(evt.target);
+        }
+         
+        var tmpl = _.template(eval(elem));
 
 
 
-      //UNCOMMENT AFTER IMPLEMENTING SIGNUP
-      //var user_model = (id != "personal_details") ? window.current_doctor : window.current_user;
+        var inserted = $(tmpl({ id: (++this.field_count[elem]) })).attr("class", "field").insertBefore(target.parent().parent()).show();
+        inserted.find(".chosen_temp").removeClass("chosen_temp").addClass("chosen_simple").chosen();
+        window.autocomplete_ajax_chosen();
 
-      if (!window.datas) window.datas = new DataCollection();
-      var form_view = new ProfileFormView({collection: window.datas, url: id, el: el, template: template});
-
-      var $tab = $('a[href="#create_profile/' + id + '"]').parent('dd'),
-          $activeTab = $tab.closest('dl').find('dd.active');
-
-      var contentLocation = "#" + id + 'Tab';
-      //Show Tab Content
-      $(contentLocation).closest('.tabs-content').children('li').removeClass('active').hide();
-      $(contentLocation).css('display', 'block').addClass('active');
-      
-      $activeTab.removeClass('active');
-      $tab.addClass('active');
-
-       // Datepicker
-      $( ".datepicker" ).datepicker({
+        console.log(inserted.find(".datepicker"))
+        inserted.find(".datepicker_temp").removeClass("datepicker_temp").datepicker({
           changeMonth: true,
           changeYear: true,
-          yearRange: "1910:2012",
-          defaultDate: "-10y",
+          yearRange: "1910:2020",
+          defaultDate: "-30y",
           dateFormat: 'dd-mm-yy'
         });
 
-      $( ".timepicker" ).timepicker({
+        inserted.find(".timepicker_temp").removeClass("timepicker_temp").timepicker({
           showPeriod: true,
           showLeadingZero: true
-      });
+        });
 
-      $(".chosen_simple").chosen();
-      window.autocomplete_ajax_chosen();
-
-      $(".open_modal").live("click", function() {
-        var elem = $(this).data("modal")
-        $("#" + elem).reveal();
         return false;
-      });
+      },
 
-      var footer_view = new FooterView();
-    },
+      // Check if a doctor with the given ID exists
+      check_existing_doctor: function() {
+        var doctor = window.current_user,
+            that = this;
+        doctor.fetch({
+          success: function(model) {
+            that.parse_doctor_data(model.toJSON());
+          }
+        });
 
-    faq: function() {
-      //load up accordian
-      
+      },
 
-      var faq_view = new FaqView();
-      // hide regular header
-      $(".header_wrapper").hide();
-    }
+      // Parse the received JSON into a simple object with appropropriate 'name' field
+      parse_doctor_data: function(model) {
+        console.log(model);
+        var data = {};
+        for(var obj in model) {
+          // Nested Data eg: Consult locations, experiences
+          if(model[obj].constructor == Array) {
+            
+            // Set the initial partial fields counter
+            var field = obj.toLowerCase() + "_field";
+            if(this.field_count[field]) this.field_count[field] = model[obj].length;
 
-  });
+            // Fill Consult Locations separately because of difference in names
+            if(obj == 'Docconsultlocation') this.field_count['consultation_field'] = model['Docconsultlocation'].length
 
-  var initialize = function(){
-    window.app = new AppRouter;
-    Backbone.history.start();
-  };
-  return {
-    initialize: initialize
-  };
+
+            // Separate out the DocSpecLinks to use in Multiple-select Chosen
+            if(obj == 'Docspeclink') {
+              var temp = ""
+              for(var i = 0; i < model[obj].length; i++) {
+                temp += (model[obj][i]['specialty_id'] + ", ");
+              }
+
+              data["data[Docspeclink]"] = temp;
+
+            } else {
+              for(var i = 0; i < model[obj].length; i++) {
+                for(key in model[obj][i]) {
+                  if(typeof model[obj][i][key] != 'object') {
+                    data[ 'data[' + obj + '][' + i + '][' + key + ']' ] = model[obj][i][key];
+                  }
+                } 
+              }  
+            }
+            
+          } else if(model[obj].constructor == Object) {
+            // Doctor Profile
+            for(key in model[obj]) {
+              data[ 'data[' + obj + '][' + key + ']' ] = model[obj][key];
+            }
+          }
+        }
+
+        console.log(this.field_count);
+
+        this.put_into_form(data);
+
+        console.log(data);
+      },
+
+
+
+    
+
+    });
+
+  return ProfileFormView;
 });
